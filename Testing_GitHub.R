1bef4bbc44f9ea4d7777c88e029697d3937fd781
datastufff <hmesba.ame@gmail.com>
2021-03-23 14:10:52 -0700

    Added a few lines of code to refamiliarize myself with R syntax.

--- Testing_GitHub.R
+++ Testing_GitHub.R
@@ -1,3 +1,12 @@
 print("This file was created withing RStudio")
 
 print("And now it lives on GitHub --Magic!")
+
+
+randomtable <- matrix(data = NA, nrow = 10, ncol = 10)
+randomtable[1,1]="Hamza"
+
+randomtable
+
+i <-  10
+randomtable[1,3]=i
